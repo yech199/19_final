a6da8b090fcf7f87b6fbc1d0efa6ed339ac9effb
Silja Sandersen <silja.sandersen@hotmail.com>
2022-01-06 16:49:11 +0100

    Update MovementCardTest.java
    Lavet tests til MovementCardTest

--- src/test/java/Model/ChanceCards/MovementCardTest.java
+++ src/test/java/Model/ChanceCards/MovementCardTest.java
@@ -1,11 +1,17 @@
 package Model.ChanceCards;
 
 
+import Model.Fields.ChanceField;
+import Model.Fields.Field;
+import Model.Fields.ShippingField;
+import Model.Fields.StartField;
 import Model.GameBoard;
 import Model.Player;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.awt.*;
 
 public class MovementCardTest {
     private Player player;
@@ -56,5 +62,33 @@ public class MovementCardTest {
         ChanceCard movementCard = new MovementCard("", "", index, MovementCard.MovementType.INDEX);
         movementCard.cardAction(player, gameBoard);
         Assert.assertEquals(index, player.getCurrentPos());
+
+        player.setCurrentPos(0);
+        index = 3;
+        movementCard = new MovementCard("", "", index, MovementCard.MovementType.NUMBER);
+        movementCard.cardAction(player, gameBoard);
+        Assert.assertEquals(3, player.getCurrentPos());
+    }
+
+    @Test
+    public void testPlayerMovesToNearest() {
+        ChanceCard[] chanceCards = new ChanceCard[]{
+                new MovementCard("", "", -1, MovementCard.MovementType.NEAREST)
+        };
+
+        Field[] fields = new Field[]{
+                new StartField("", "", "", Color.BLACK),
+                new ShippingField("", "", "", 0),
+                new ShippingField("", "", "", 0),
+                new ChanceField()
+        };
+
+        gameBoard = new GameBoard(fields, chanceCards);
+        chanceCards[0].cardAction(player, gameBoard);
+        Assert.assertEquals(1, player.getCurrentPos());
+
+        player.setCurrentPos(3);
+        chanceCards[0].cardAction(player, gameBoard);
+        Assert.assertEquals(1, player.getCurrentPos());
     }
 }
