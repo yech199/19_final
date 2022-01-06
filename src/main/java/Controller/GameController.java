11ec145f71b509ef3a27fbcf998b11bb52920327
Silja Sandersen <silja.sandersen@hotmail.com>
2022-01-06 14:26:29 +0100

    Update GameController.java
    Merge med develop

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -143,6 +143,27 @@ public class GameController {
                     player.addAmountToBalance(-rent);
                     breweryField.owner.addAmountToBalance(rent);
                 }
+            } else if (ownableField instanceof ShippingField shippingField) {
+                if (shippingField.owner != null) {
+                    int rent = shippingField.rent * 1;
+                    player.addAmountToBalance(-rent);
+                    shippingField.owner.addAmountToBalance(rent);
+                }
+                else if (shippingField.owner != null) {
+                    int rent = shippingField.rent * 2;
+                    player.addAmountToBalance(-rent);
+                    shippingField.owner.addAmountToBalance(rent);
+                }
+                else if (shippingField.owner != null) {
+                    int rent = shippingField.rent * 4;
+                    player.addAmountToBalance(-rent);
+                    shippingField.owner.addAmountToBalance(rent);
+                }
+                else if (shippingField.owner != null) {
+                    int rent = shippingField.rent * 8;
+                    player.addAmountToBalance(-rent);
+                    shippingField.owner.addAmountToBalance(rent);
+                }
             }
 
             if (ownableField.owner == null) {
@@ -156,14 +177,24 @@ public class GameController {
                 // Tjekker om ejeren af det nyligt købte felt også ejer det andet af samme farve
                 //------------------------------------------------------------------------------------------------------
                 // FIXME
-                // if (ownsBoth(propertyField)) {
-                //
-                //     PropertyField[] tmpFields = gameBoard.getPair(propertyField.backgroundColor);
-                //     tmpFields[0].rent += tmpFields[0].rent;
-                //     tmpFields[1].rent += tmpFields[1].rent;
-                //     // Tilføjer renten til sig selv for at fordoble den.
-                //     // OBS!! Denne metode er kun brugbar når ejeren ikke kan ændres.
-                // }
+                if (landedOn instanceof PropertyField propertyField) {
+                    if (ownsAll(propertyField)) {
+                        if (propertyField.getAmountOfHouses() == 0) {
+                            PropertyField[] tmpFields = gameBoard.getFieldGroup(propertyField.backgroundColor);
+                            for (PropertyField tmpField : tmpFields) {
+                                tmpField.rent += tmpField.rent;
+                            }
+                            if (guiController.getUserButtonPressed("Du ejer alle felter af denne farve. " +
+                                    "Vil du købe et hus for 4.000 kr til dette felt?", "Ja", "Nej").equals("Ja")) {
+                                propertyField.addHouse(1);
+                            }
+                        }
+
+                        // Tilføjer renten til sig selv for at fordoble den.
+                        // OBS!! Denne metode er kun brugbar når ejeren ikke kan ændres.
+                    }
+
+                }
             } else guiController.updatePlayer(ownableField.owner);
 
 
@@ -203,19 +234,23 @@ public class GameController {
      * @return boolean output der siger om ejeren har begge felter med denne farve eller ej.
      */
     // FIXME
-    public boolean ownsBoth(PropertyField field) {
+    public boolean ownsAll(PropertyField field) {
 
-        Field[] tmpFields = gameBoard.getPair(field.backgroundColor);
+        Field[] tmpFields = gameBoard.getFieldGroup(field.backgroundColor);
 
         //--------------------------------------------------------------------------------------------------------------
         // Tjekker om nogle af felterne ikke har en ejer, da dette er nødvendigt for at kunne sammenligne i
         // return statementet.
         //--------------------------------------------------------------------------------------------------------------
-        if (((PropertyField) tmpFields[0]).owner == null || ((PropertyField) tmpFields[1]).owner == null) {
-            return false;
-        }
-
-        return ((PropertyField) tmpFields[0]).owner == ((PropertyField) tmpFields[1]).owner;
+        if (tmpFields.length == 3) {
+            if (((PropertyField) tmpFields[0]).owner == null || ((PropertyField) tmpFields[1]).owner == null ||
+                    ((PropertyField) tmpFields[2]).owner == null) {
+                return false;
+            }
+            //tjekker om ejeren af første, andet og tredje felt er den samme. Hvis ikke returnerer den false
+            return ((PropertyField) tmpFields[0]).owner == ((PropertyField) tmpFields[1]).owner &&
+                    ((PropertyField) tmpFields[2]).owner == ((PropertyField) tmpFields[1]).owner;
+        } else return ((PropertyField) tmpFields[0]).owner == ((PropertyField) tmpFields[1]).owner;
     }
 
     /**
